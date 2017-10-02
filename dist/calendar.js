b2df211786156f4c8362a4a84641a60034f9611a
cebo <cebo@statim.fr>
2017-10-02 14:02:25 +0200

    add dist

---
+++ dist/calendar.js
@@ -0,0 +1,153 @@
+"use strict";
+var core_1 = require('@angular/core');
+var calendar_service_1 = require('./calendar.service');
+(function (Step) {
+    Step[Step["QuarterHour"] = 15] = "QuarterHour";
+    Step[Step["HalfHour"] = 30] = "HalfHour";
+    Step[Step["Hour"] = 60] = "Hour";
+})(exports.Step || (exports.Step = {}));
+var Step = exports.Step;
+var CalendarComponent = (function () {
+    function CalendarComponent(calendarService, appLocale) {
+        this.calendarService = calendarService;
+        this.appLocale = appLocale;
+        this.eventSource = [];
+        this.calendarMode = 'month';
+        this.formatDay = 'd';
+        this.formatDayHeader = 'EEE';
+        this.formatDayTitle = 'MMMM dd, yyyy';
+        this.formatWeekTitle = 'MMMM yyyy, Week $n';
+        this.formatMonthTitle = 'MMMM yyyy';
+        this.formatWeekViewDayHeader = 'EEE d';
+        this.formatHourColumn = 'j';
+        this.showEventDetail = true;
+        this.startingDayMonth = 0;
+        this.startingDayWeek = 0;
+        this.allDayLabel = 'all day';
+        this.noEventsLabel = 'No Events';
+        this.queryMode = 'local';
+        this.step = Step.Hour;
+        this.autoSelect = true;
+        this.dir = "";
+        this.scrollToHour = 0;
+        this.preserveScrollPosition = false;
+        this.lockSwipeToPrev = false;
+        this.lockSwipes = false;
+        this.locale = "";
+        this.onCurrentDateChanged = new core_1.EventEmitter();
+        this.onRangeChanged = new core_1.EventEmitter();
+        this.onEventSelected = new core_1.EventEmitter();
+        this.onTimeSelected = new core_1.EventEmitter();
+        this.onTitleChanged = new core_1.EventEmitter();
+        this.hourParts = 1;
+        this.locale = appLocale;
+    }
+    Object.defineProperty(CalendarComponent.prototype, "currentDate", {
+        get: function () {
+            return this._currentDate;
+        },
+        set: function (val) {
+            if (!val) {
+                val = new Date();
+            }
+            this._currentDate = val;
+            this.calendarService.setCurrentDate(val, true);
+            this.onCurrentDateChanged.emit(this._currentDate);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    CalendarComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        if (this.autoSelect) {
+            if (this.autoSelect.toString() === 'false') {
+                this.autoSelect = false;
+            }
+            else {
+                this.autoSelect = true;
+            }
+        }
+        this.hourParts = 60 / this.step;
+        this.calendarService.queryMode = this.queryMode;
+        this.currentDateChangedFromChildrenSubscription = this.calendarService.currentDateChangedFromChildren$.subscribe(function (currentDate) {
+            _this._currentDate = currentDate;
+            _this.onCurrentDateChanged.emit(currentDate);
+        });
+    };
+    CalendarComponent.prototype.ngOnDestroy = function () {
+        if (this.currentDateChangedFromChildrenSubscription) {
+            this.currentDateChangedFromChildrenSubscription.unsubscribe();
+            this.currentDateChangedFromChildrenSubscription = null;
+        }
+    };
+    CalendarComponent.prototype.rangeChanged = function (range) {
+        this.onRangeChanged.emit(range);
+    };
+    CalendarComponent.prototype.eventSelected = function (event) {
+        this.onEventSelected.emit(event);
+    };
+    CalendarComponent.prototype.timeSelected = function (timeSelected) {
+        this.onTimeSelected.emit(timeSelected);
+    };
+    CalendarComponent.prototype.titleChanged = function (title) {
+        this.onTitleChanged.emit(title);
+    };
+    CalendarComponent.prototype.loadEvents = function () {
+        this.calendarService.loadEvents();
+    };
+    CalendarComponent.decorators = [
+        { type: core_1.Component, args: [{
+                    selector: 'calendar',
+                    template: "\n        <template #monthviewDefaultDisplayEventTemplate let-view=\"view\" let-row=\"row\" let-col=\"col\">\n            {{view.dates[row*7+col].label}}\n        </template>\n        <template #monthviewDefaultEventDetailTemplate let-showEventDetail=\"showEventDetail\" let-selectedDate=\"selectedDate\" let-noEventsLabel=\"noEventsLabel\">\n            <ion-list class=\"event-detail-container\" has-bouncing=\"false\" *ngIf=\"showEventDetail\" overflow-scroll=\"false\">\n                <ion-item *ngFor=\"let event of selectedDate?.events\" (click)=\"eventSelected(event)\">\n                        <span *ngIf=\"!event.allDay\" class=\"monthview-eventdetail-timecolumn\">{{event.startTime|date: 'HH:mm'}}\n                            -\n                            {{event.endTime|date: 'HH:mm'}}\n                        </span>\n                    <span *ngIf=\"event.allDay\" class=\"monthview-eventdetail-timecolumn\">{{allDayLabel}}</span>\n                    <span class=\"event-detail\">  |  {{event.title}}</span>\n                </ion-item>\n                <ion-item *ngIf=\"selectedDate?.events.length==0\">\n                    <div class=\"no-events-label\">{{noEventsLabel}}</div>\n                </ion-item>\n            </ion-list>\n        </template>\n        <template #defaultAllDayEventTemplate let-displayEvent=\"displayEvent\">\n            <div class=\"calendar-event-inner\">{{displayEvent.event.title}}</div>\n        </template>\n        <template #defaultNormalEventTemplate let-displayEvent=\"displayEvent\">\n            <div class=\"calendar-event-inner\">{{displayEvent.event.title}}</div>\n        </template>\n\n        <div [ngSwitch]=\"calendarMode\" class=\"{{calendarMode}}view-container\">\n            <monthview *ngSwitchCase=\"'month'\"\n                [formatDay]=\"formatDay\"\n                [formatDayHeader]=\"formatDayHeader\"\n                [formatMonthTitle]=\"formatMonthTitle\"\n                [startingDayMonth]=\"startingDayMonth\"\n                [showEventDetail]=\"showEventDetail\"\n                [noEventsLabel]=\"noEventsLabel\"\n                [autoSelect]=\"autoSelect\"\n                [eventSource]=\"eventSource\"\n                [markDisabled]=\"markDisabled\"\n                [monthviewDisplayEventTemplate]=\"monthviewDisplayEventTemplate||monthviewDefaultDisplayEventTemplate\"\n                [monthviewInactiveDisplayEventTemplate]=\"monthviewInactiveDisplayEventTemplate||monthviewDefaultDisplayEventTemplate\"\n                [monthviewEventDetailTemplate]=\"monthviewEventDetailTemplate||monthviewDefaultEventDetailTemplate\"\n                [locale]=\"locale\"\n                [dateFormatter]=\"dateFormatter\"\n                [dir]=\"dir\"\n                [lockSwipeToPrev]=\"lockSwipeToPrev\"\n                [lockSwipes]=\"lockSwipes\"\n                (onRangeChanged)=\"rangeChanged($event)\"\n                (onEventSelected)=\"eventSelected($event)\"\n                (onTimeSelected)=\"timeSelected($event)\"\n                (onTitleChanged)=\"titleChanged($event)\">\n            </monthview>\n            <weekview *ngSwitchCase=\"'week'\"\n                [formatWeekTitle]=\"formatWeekTitle\"\n                [formatWeekViewDayHeader]=\"formatWeekViewDayHeader\"\n                [formatHourColumn]=\"formatHourColumn\"\n                [startingDayWeek]=\"startingDayWeek\"\n                [allDayLabel]=\"allDayLabel\"\n                [hourParts]=\"hourParts\"\n                [eventSource]=\"eventSource\"\n                [markDisabled]=\"markDisabled\"\n                [weekviewAllDayEventTemplate]=\"weekviewAllDayEventTemplate||defaultAllDayEventTemplate\"\n                [weekviewNormalEventTemplate]=\"weekviewNormalEventTemplate||defaultNormalEventTemplate\"\n                [locale]=\"locale\"\n                [dateFormatter]=\"dateFormatter\"\n                [dir]=\"dir\"\n                [scrollToHour]=\"scrollToHour\"\n                [preserveScrollPosition]=\"preserveScrollPosition\"\n                [lockSwipeToPrev]=\"lockSwipeToPrev\"\n                [lockSwipes]=\"lockSwipes\"\n                (onRangeChanged)=\"rangeChanged($event)\"\n                (onEventSelected)=\"eventSelected($event)\"\n                (onTimeSelected)=\"timeSelected($event)\"\n                (onTitleChanged)=\"titleChanged($event)\">\n            </weekview>\n            <dayview *ngSwitchCase=\"'day'\"\n                [formatDayTitle]=\"formatDayTitle\"\n                [formatHourColumn]=\"formatHourColumn\"\n                [allDayLabel]=\"allDayLabel\"\n                [hourParts]=\"hourParts\"\n                [eventSource]=\"eventSource\"\n                [markDisabled]=\"markDisabled\"\n                [dayviewAllDayEventTemplate]=\"dayviewAllDayEventTemplate||defaultAllDayEventTemplate\"\n                [dayviewNormalEventTemplate]=\"dayviewNormalEventTemplate||defaultNormalEventTemplate\"\n                [locale]=\"locale\"\n                [dateFormatter]=\"dateFormatter\"\n                [dir]=\"dir\"\n                [scrollToHour]=\"scrollToHour\"\n                [preserveScrollPosition]=\"preserveScrollPosition\"\n                [lockSwipeToPrev]=\"lockSwipeToPrev\"\n                [lockSwipes]=\"lockSwipes\"\n                (onRangeChanged)=\"rangeChanged($event)\"\n                (onEventSelected)=\"eventSelected($event)\"\n                (onTimeSelected)=\"timeSelected($event)\"\n                (onTitleChanged)=\"titleChanged($event)\">\n            </dayview>\n        </div>\n    ",
+                    styles: ["\n        :host > div { height: 100%; }\n\n        .event-detail-container {\n          border-top: 2px darkgrey solid;\n        }\n\n        .no-events-label {\n          font-weight: bold;\n          color: darkgrey;\n          text-align: center;\n        }\n\n        .event-detail {\n          cursor: pointer;\n          white-space: nowrap;\n          text-overflow: ellipsis;\n        }\n\n        .monthview-eventdetail-timecolumn {\n          width: 110px;\n          overflow: hidden;\n        }\n\n        .calendar-event-inner {\n          overflow: hidden;\n          background-color: #3a87ad;\n          color: white;\n          height: 100%;\n          width: 100%;\n          padding: 2px;\n          line-height: 15px;\n        }\n\n        @media (max-width: 750px) {\n          .calendar-event-inner {\n            font-size: 12px;\n          }\n        }\n    "],
+                    providers: [calendar_service_1.CalendarService]
+                },] },
+    ];
+    CalendarComponent.ctorParameters = function () { return [
+        { type: calendar_service_1.CalendarService, },
+        { type: undefined, decorators: [{ type: core_1.Inject, args: [core_1.LOCALE_ID,] },] },
+    ]; };
+    CalendarComponent.propDecorators = {
+        'currentDate': [{ type: core_1.Input },],
+        'eventSource': [{ type: core_1.Input },],
+        'calendarMode': [{ type: core_1.Input },],
+        'formatDay': [{ type: core_1.Input },],
+        'formatDayHeader': [{ type: core_1.Input },],
+        'formatDayTitle': [{ type: core_1.Input },],
+        'formatWeekTitle': [{ type: core_1.Input },],
+        'formatMonthTitle': [{ type: core_1.Input },],
+        'formatWeekViewDayHeader': [{ type: core_1.Input },],
+        'formatHourColumn': [{ type: core_1.Input },],
+        'showEventDetail': [{ type: core_1.Input },],
+        'startingDayMonth': [{ type: core_1.Input },],
+        'startingDayWeek': [{ type: core_1.Input },],
+        'allDayLabel': [{ type: core_1.Input },],
+        'noEventsLabel': [{ type: core_1.Input },],
+        'queryMode': [{ type: core_1.Input },],
+        'step': [{ type: core_1.Input },],
+        'autoSelect': [{ type: core_1.Input },],
+        'markDisabled': [{ type: core_1.Input },],
+        'monthviewDisplayEventTemplate': [{ type: core_1.Input },],
+        'monthviewInactiveDisplayEventTemplate': [{ type: core_1.Input },],
+        'monthviewEventDetailTemplate': [{ type: core_1.Input },],
+        'weekviewAllDayEventTemplate': [{ type: core_1.Input },],
+        'weekviewNormalEventTemplate': [{ type: core_1.Input },],
+        'dayviewAllDayEventTemplate': [{ type: core_1.Input },],
+        'dayviewNormalEventTemplate': [{ type: core_1.Input },],
+        'dateFormatter': [{ type: core_1.Input },],
+        'dir': [{ type: core_1.Input },],
+        'scrollToHour': [{ type: core_1.Input },],
+        'preserveScrollPosition': [{ type: core_1.Input },],
+        'lockSwipeToPrev': [{ type: core_1.Input },],
+        'lockSwipes': [{ type: core_1.Input },],
+        'locale': [{ type: core_1.Input },],
+        'onCurrentDateChanged': [{ type: core_1.Output },],
+        'onRangeChanged': [{ type: core_1.Output },],
+        'onEventSelected': [{ type: core_1.Output },],
+        'onTimeSelected': [{ type: core_1.Output },],
+        'onTitleChanged': [{ type: core_1.Output },],
+    };
+    return CalendarComponent;
+}());
+exports.CalendarComponent = CalendarComponent;
+//# sourceMappingURL=calendar.js.map
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var calendar_module_1 = require('./calendar.module');
+exports.NgCalendarModule = calendar_module_1.NgCalendarModule;
+//# sourceMappingURL=index.js.map
