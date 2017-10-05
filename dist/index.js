74d788231bcd367fd5459c05daf27b13603615b7
brun <brun@statim.fr>
2017-10-05 08:38:56 +0200

    fix(package): correction pour npm pack

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var calendar_module_1 = require("./calendar.module");
-exports.NgCalendarModule = calendar_module_1.NgCalendarModule;
-//# sourceMappingURL=index.js.map
