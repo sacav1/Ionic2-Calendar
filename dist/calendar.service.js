0959b666972000f9e2fbe2636217ada1741d7a75
brun <brun@statim.fr>
2017-10-05 08:36:51 +0200

    feat(package): passage à ionic 3.7.1

--- dist/calendar.service.js
+++ dist/calendar.service.js
@@ -1,13 +1,4 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var Subject_1 = require("rxjs/Subject");
@@ -123,11 +114,11 @@ var CalendarService = (function () {
     CalendarService.prototype.loadEvents = function () {
         this.eventSourceChanged.next();
     };
+    CalendarService.decorators = [
+        { type: core_1.Injectable },
+    ];
+    CalendarService.ctorParameters = function () { return []; };
     return CalendarService;
 }());
-CalendarService = __decorate([
-    core_1.Injectable(),
-    __metadata("design:paramtypes", [])
-], CalendarService);
 exports.CalendarService = CalendarService;
 //# sourceMappingURL=calendar.service.js.map
--- dist/init-position-scroll.js
+++ dist/init-position-scroll.js
@@ -9,22 +9,13 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var ionic_angular_1 = require("ionic-angular");
 var core_1 = require("@angular/core");
 var initPositionScrollComponent = (function (_super) {
     __extends(initPositionScrollComponent, _super);
     function initPositionScrollComponent(el) {
-        var _this = _super.call(this, el) || this;
+        var _this = _super.call(this) || this;
         _this.onScroll = new core_1.EventEmitter();
         _this.listenerAttached = false;
         _this.element = el;
@@ -55,26 +46,21 @@ var initPositionScrollComponent = (function (_super) {
             this.scrollContent.removeEventListener('scroll', this.handler);
         }
     };
+    initPositionScrollComponent.decorators = [
+        { type: core_1.Component, args: [{
+                    selector: 'init-position-scroll',
+                    template: "\n        <ion-scroll scrollY=\"true\" zoom=\"false\" style=\"height:100%\">\n            <ng-content></ng-content>\n        </ion-scroll>\n    "
+                },] },
+    ];
+    initPositionScrollComponent.ctorParameters = function () { return [
+        { type: core_1.ElementRef, },
+    ]; };
+    initPositionScrollComponent.propDecorators = {
+        'initPosition': [{ type: core_1.Input },],
+        'emitEvent': [{ type: core_1.Input },],
+        'onScroll': [{ type: core_1.Output },],
+    };
     return initPositionScrollComponent;
 }(ionic_angular_1.Scroll));
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", Number)
-], initPositionScrollComponent.prototype, "initPosition", void 0);
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", Boolean)
-], initPositionScrollComponent.prototype, "emitEvent", void 0);
-__decorate([
-    core_1.Output(),
-    __metadata("design:type", Object)
-], initPositionScrollComponent.prototype, "onScroll", void 0);
-initPositionScrollComponent = __decorate([
-    core_1.Component({
-        selector: 'init-position-scroll',
-        template: "\n        <ion-scroll scrollY=\"true\" zoom=\"false\" style=\"height:100%\">\n            <ng-content></ng-content>\n        </ion-scroll>\n    "
-    }),
-    __metadata("design:paramtypes", [core_1.ElementRef])
-], initPositionScrollComponent);
 exports.initPositionScrollComponent = initPositionScrollComponent;
 //# sourceMappingURL=init-position-scroll.js.map
